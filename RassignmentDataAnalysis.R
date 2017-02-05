2087dc33069460759ab9c4afd41ebd626d40bed2
Alexandros Ioannidis <alexioanid@yahoo.gr>
2017-02-05 21:01:42 +0000

    adding files

--- RassignmentDataAnalysis.R
+++ RassignmentDataAnalysis.R
@@ -1,3 +1,29 @@
+library(car)
+# read whole dataset 748 instances
+train <-  read.csv('C:/Users/Alexandros/Dropbox/MSc/2nd Semester/Data analysis/Assignment/transfusiondata.csv')
+#-------------------------------------------------------------------
+unique(sort(train$Recency..months.))
+index <- (train$Recency..months. == train$Time..months.) & train$Frequency..times. > 1
+dim(train[index,])
+#--------------------------------------------------------------------
+scatterplot(train$Recency..months. ~ train$whether.he.she.donated.blood.in.March.2007, data=train, 
+            xlab="Recency", ylab="Donation", 
+            main="Scatter Plot")
+#------------------------------------------------
+cor_df <- data.frame(train) # convert file to dataframe
+cor_df$CustID <- NULL
+cor_df$Time..months.<- NULL
+cor_df$whether.he.she.donated.blood.in.March.2007 <- NULL
+M <- cor(cor_df) # get correlations
+install.packages("corrplot")
+library("corrplot") #package corrplot
+corrplot(M, method = "circle") #plot matrix
+#------------------------------------------------
+sd(train$Recency..months.)
+sd(train$Frequency..times.)
+par(mar = rep(2, 4))
+plot(train$Recency..months.,train$whether.he.she.donated.blood.in.March.2007)
+plot(train$Frequency..times.,train$whether.he.she.donated.blood.in.March.2007)
 # read training and testing datasets
 traindata <-  read.csv('C:/Users/Alexandros/Dropbox/MSc/2nd Semester/Data analysis/Assignment/transfusion.csv')
 testdata <-  read.csv('C:/Users/Alexandros/Dropbox/MSc/2nd Semester/Data analysis/Assignment/test.csv')
@@ -39,7 +65,7 @@ for(i in 1:nrow(matrix_train)) {
     matrix_train[i,6] <- 2
   } else {
     matrix_train[i,6] <- 1
-    }
+  }
 }
 #----------------------------------------------------
 for(i in 1:nrow(matrix_test)) {
@@ -110,36 +136,50 @@ matrix_test <- as.matrix(sapply(sorted_dftest2, as.numeric))
 #--------------------------------------------------------------------
 i<-0
 count_train_predicted_donations <- 0
+counter_train <- 0
 for(i in 1:nrow(matrix_train)) {
   if ((matrix_train[i,8] >= 4) & (matrix_train[i,5] == 1)) {
-    count_train_predicted_donations = count_train_predicted_donations + 1
+    count_train_predicted_donations = count_train_predicted_donations + 1}
+  if (matrix_train[i,8] >= 4) {
+    counter_train <- counter_train + 1
   }
 }
 #------------------------------------------------------------
 count_test_predicted_donations <- 0
+counter_test<-0
 for(i in 1:nrow(matrix_test)) {
   if ((matrix_test[i,8] >= 4) & (matrix_test[i,5] == 1)) {
-    count_test_predicted_donations = count_test_predicted_donations + 1
+    count_test_predicted_donations = count_test_predicted_donations + 1 }
+  if (matrix_test[i,8] >= 4) {
+    counter_test <- counter_test + 1
   }
 }
 #-------------------------------------------------------------
 count_train_all_donations <- 0
 for(i in 1:nrow(matrix_train)) {
   if (matrix_train[i,5] == 1) {
-    count_train_all_donations = count_train_all_donations + 1
-  }
+    count_train_all_donations = count_train_all_donations + 1  }
 }
 #----------------------------------------------------------------
 count_test_all_donations <- 0
 for(i in 1:nrow(matrix_test)) {
   if (matrix_test[i,5] == 1) {
-    count_test_all_donations = count_test_all_donations + 1
-  }
+    count_test_all_donations = count_test_all_donations + 1  }
 }
 #-----------------------------------------------------
-train_accuracy <- (count_train_predicted_donations/count_train_all_donations)
-test_accuracy <- (count_test_predicted_donations / count_test_all_donations )
-err = abs(test_accuracy-train_accuracy)
-cat("Accuracy with training dataset: ", train_accuracy)
-cat("Accuracy with testing dataset: ", test_accuracy)
-cat("Error: ", err)
+train_donations_accuracy <- (count_train_predicted_donations/count_train_all_donations)
+test_donations_accuracy <- (count_test_predicted_donations / count_test_all_donations )
+donation_err = abs(test_donations_accuracy - train_donations_accuracy)
+cat("Accuracy for predicting donations with training dataset: ", train_donations_accuracy)
+cat("Accuracy for predicting donations with testing dataset: ", test_donations_accuracy)
+cat("Predicting Donations Error: ", donation_err)
+#-----------------------------------------------------
+dftrain <- data.frame(matrix_train)
+dftrain_final <- dftrain[c(1:counter_train),1:8]
+#----------------------------------------------------
+dftest <- data.frame(matrix_test)
+dftest_final <- dftest[c(1:counter_test),1:8]
+#------------------------------------------------------
+write.csv(dftrain_final, file = "C:\\Users\\Alexandros\\Dropbox\\MSc\\2nd Semester\\Data analysis\\Assignment\\train_output.csv", row.names = FALSE)
+write.csv(dftest_final, file = "C:\\Users\\Alexandros\\Dropbox\\MSc\\2nd Semester\\Data analysis\\Assignment\\test_output.csv", row.names = FALSE)
+#--------------------------------------------------------
